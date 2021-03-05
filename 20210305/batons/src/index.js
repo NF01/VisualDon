6ced2eec4520d6509af65dcfe94af43e10358051
NF01 <72254017+NF01@users.noreply.github.com>
2021-03-05 16:36:06 +0100

    Add files via upload

---
+++ 20210305/batons/src/index.js
@@ -0,0 +1,71 @@
+import {
+  axisLeft,
+  select,
+  scaleLinear,
+  max,
+} from 'd3'
+
+const DATA =  [
+  {
+      Gwh: 125.12,
+      Commune: "Lausanne"
+  },
+  {
+      Gwh: 36.86,
+      Commune: "Montreux"
+  },
+  {
+      Gwh: 36.25,
+      Commune: "Yverdon-les-Bains"
+  },
+  {
+      Gwh: 33.87,
+      Commune: "Ollon"
+  }
+]
+
+const WIDTH = 1000
+const HEIGHT = 500
+const MARGIN = 5
+const MARGIN_LEFT = 50
+const MARGIN_BOTTOM = 50
+const BAR_WIDTH = (WIDTH - MARGIN_LEFT) / DATA.length
+
+const svg = select('body')
+  .append('svg')
+  .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
+
+const yScale = scaleLinear()
+  .domain([0, max(DATA, d => d.Gwh)])
+  .range([HEIGHT - MARGIN_BOTTOM, 0])
+
+
+const g = svg.append('g')
+  .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
+
+g.selectAll('rect')
+  .data(DATA)
+  .enter()
+  .append('rect')
+  .attr('x', (d, i) =>  i * BAR_WIDTH)
+  .attr('width', BAR_WIDTH - MARGIN)
+  .attr('y', d => yScale(d.Gwh))
+  .attr('height', d => HEIGHT - MARGIN_BOTTOM - yScale(d.Gwh))
+  .attr('fill', 'steelblue')
+
+g.selectAll('text')
+  .data(DATA)
+  .enter()
+  .append('text')
+  .text(d => d.Commune)
+  .attr('x', (d, i) =>  i * BAR_WIDTH + BAR_WIDTH / 2)
+  .attr('y', HEIGHT - MARGIN_BOTTOM / 2)
+  .attr('text-anchor', 'middle')
+
+const axisY = axisLeft().scale(yScale)
+  .tickFormat(d => `${d}Gwh`)
+  .ticks(3)
+
+svg.append('g')
+  .attr('transform', `translate(${MARGIN_LEFT -3})`)
+  .call(axisY)
